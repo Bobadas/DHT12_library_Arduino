fa6d77d25e529a8e2dd399387202ba218633f2a4
Bobadas <Bobadas@users.noreply.github.com>
2016-07-22 10:07:53 +0200

    Update DHT12.h

--- DHT12.h
+++ DHT12.h
@@ -1,7 +1,7 @@
 /*
 	DHT12.h - Library for DHT12 sensor.
 	v0.0.1 Beta
-	Created by Bobodas, July 30,2016.
+	Created by Bobadas, July 30,2016.
 	Released into the public domain.
 */
 #ifndef DHT12_h
